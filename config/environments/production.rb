3b2d309015112cafc1fb6044f4cb804fe24fb555
Eric <edupre13@gmail.com>
2017-10-15 16:26:24 +0000

    remake heroku site 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'https://frozen-depths-31275.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://pinterestclone1327.herokuapp.com' }
   
   config.paperclip_defaults = {
   storage: :s3,
